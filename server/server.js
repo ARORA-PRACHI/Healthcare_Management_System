1944ad6b65fe3c10cce74727b7d672f2ead5570f
Prachi <prachi659.be22@chitkara.edu.in>
2024-11-25 02:48:25 +0530

    HBS

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const dotenv = require("dotenv").config();
+const hbs = require('hbs');
+
+const errorHandler = require("./middlewares/errorHandler..js");
 const connectDb = require("./config/dbConnection.js");
+
 
 // Import the routers
 const userRouter = require("./routers/userRouter");
@@ -11,12 +16,35 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
+//hbs
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
+app.get('/home',(req,res)=>{
+    res.render('home',{
+        username: "SM",
+        posts: "Chairman"
+    })
+})
+app.get('/allusers',(req,res)=>{
+    res.render('allusers',{
+        data:[{name:"SM", age:21},
+            {name:"PA", age:21},
+            {name:"BM", age:0.3},
+            {name:"EA",age:0.01}
+        ]
+    })
+})
 app.get('/', (req, res) => {
     res.send("Working");
 });
+
+
 
 // Routes for users
 app.use('/api/users', userRouter);
